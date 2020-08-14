d125e6f3fdbfc542bd1d7f9c7f99f54c6273d010
Dickson Wu <33645481+DicksonWu654@users.noreply.github.com>
2020-08-14 01:21:33 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/x99px2ihf98nrqk/antthing.pkl?dl=0'
+export_file_name = 'antthing.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ["carp", "bga", "BA", "PA", "RA", "WA"]
 path = Path(__file__).parent
 
 app = Starlette()
